3b5302dffe2cf797ec99a3add93024d0dee645b2
yang20002020 <2628636987@qq.com>
2023-10-25 22:16:39 +0800

    文本插值

---
+++ vue_demo_02/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{name}}</h1>
+    <h1>{{ age >60?'老年':'青年'}}</h1>
+  </div>
+</template>
+
+<script>
+export default{
+   data(){
+     return {name:'张三',age:30};
+   }
+}
+</script>
+
+<style >
+</style>
