3d4ed4d9d978d3dcead9674c2eae448a8c9654fc
yang20002020 <2628636987@qq.com>
2023-10-26 07:46:27 +0800

    事件绑定

--- vue_demo_02/src/components/HelloWorld.vue
+++ vue_demo_02/src/components/HelloWorld.vue
@@ -2,13 +2,33 @@
   <div>
     <h1>{{name}}</h1>
     <h1>{{ age >60?'老年':'青年'}}</h1>
+
+    <div><input type="text" v-bind:value="name"></div>
+    <div><input type="text" :value="age"></div>
+    <div><img :src="src"/></div>
+
+
+    <div>
+      <input type="button" value="保存" v-on:click="handleSave"/>
+      <input type="button" value="保存" @click="handleSave"/><br>
+    </div>
   </div>
 </template>
 
 <script>
 export default{
    data(){
-     return {name:'张三',age:30};
+     return {
+       name:'张三',
+       age:30,
+       src:'https://tlisa-web.oss-cn-beijing.aliyuncs.com/7a7b56e5-444d-4ed2-9850-f4f5bb2fb174.png'
+     };
+   },
+
+   methods:{
+     handleSave(){
+        alert(this.name)
+     }
    }
 }
 </script>
