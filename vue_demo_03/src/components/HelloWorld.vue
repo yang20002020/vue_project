59bd14927d46fd02cd35751bbb6b59a7992f8be0
yang20002020 <2628636987@qq.com>
2023-10-26 08:25:01 +0800

    双向绑定

---
+++ vue_demo_03/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    双向绑定: {{name}}
+    <input type="text" v-model="name">
+    <input type="button" value="改变" @click="handleChange"/>
+  </div>
+</template>
+
+<script>
+ export default{
+     data(){
+       return {
+         name:'张三'
+       }
+     },
+     methods:{
+       handleChange(){
+         this.name='李四'
+       }
+     }
+ }
+</script>
+
+<style >
+</style>
